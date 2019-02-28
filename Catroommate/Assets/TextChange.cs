5a8e2991714c30ccd374812caa6b39c839a5e4d1
Isabella Chen <xc1338@nyu.edu>
2019-02-28 11:19:09 -0800

    added the entire unity project

---
+++ Catroommate/Assets/TextChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextChange : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    private string[] textStuff;
+
+    public void ChangeText();
+    {
+      int randNum;
+      randNum = Random.Range(0, textStuff.Length);
+
+      text.text = textStuff[randNum];
+    }
+}

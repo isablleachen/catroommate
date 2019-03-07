7c7c20fb42df0846cd265a07ae0a852dfedddec4
Isabella Chen <xc1338@nyu.edu>
2019-03-07 11:53:40 -0500

    add basic assets

---
+++ Catroommate/Assets/Scripts/ConversationManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ConversationManager : MonoBehaviour
+{
+    public TextMeshProUGUI question;
+    public string[] questions;
+
+    private void Start()
+    {
+        
+    }
+
+    public void ShowNextQuestion()
+    {
+        question.text = "";
+    }
+
+    public void ClickOnRight()
+    {
+        ShowNextQuestion();
+    }
+
+    public void ClickOnLeft()
+    {
+        ShowNextQuestion();
+    }
+}
+
